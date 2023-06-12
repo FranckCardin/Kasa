f3a985ed500689126d41d238005005d71fed1c94
Franck Cardin <franck.cardin@free.fr>
2023-06-12 19:00:39 +0200

    Modification About.js

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import "../styles/apropos.css";
+
+function About(){
+    return (
+        <main>
+            <div className="about">
+                <Header />
+                <Banner />
+                <main>
+                    <div className="about__content">
+
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </main>
+    )
+}
+
+export default About;
