b8cb8e30680ba86b137808b4ed679986377524d0
Franck Cardin <franck.cardin@free.fr>
2023-06-12 18:34:36 +0200

    Modification 404Error.js

--- src/pages/404Error.js
+++ src/pages/404Error.js
@@ -1,6 +1,18 @@
-function Notfound(){
-    const text = "Vous êtes sur la page 404 NOT FOUND" 
-    return text;
+import '../styles/404Error.css'
+import Header from "../components/Header";
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+	return (
+		<div className='not_found'>
+			<Header />
+			<div className="not_found__infos">
+				<h1 className='not_found__title'>404</h1>
+				<p className='not_found__content'>La page que vous recherchez n'existe pas.</p>
+			</div>
+				<Link className='not_found__return' to='/'>Retourner sur la page d'accueil</Link>
+		</div>
+	)
 }
 
-export default Notfound;
+export default NotFound;
