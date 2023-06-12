df9508ed989065245f043c8ac3e9864d01d5794f
Franck Cardin <franck.cardin@free.fr>
2023-06-12 11:56:04 +0200

    Modification Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
- 
+import '../styles/nav.css';
+
 function Nav(){
     return (
-        <nav className="nav">
-            <ul className="nav-list">
+        <nav className="navbar">
+            <ul className="navbar__list">
                 <li><Link to="/">Accueil</Link></li>
                 <li><Link to="Apropos">A propos</Link></li>
             </ul>
