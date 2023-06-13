c7d0621dd63aa85bd6718a0b2653b07457a5b8c7
Franck Cardin <franck.cardin@free.fr>
2023-06-13 02:51:50 +0200

    Modification Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,13 @@
 import data from "../data/data";
-import '../styles/card.css';
-
+import '../styles/card.scss';
 
 function Card(){
     return (
         //Récupération et création d'un tableau des données des logements du fichier data
           <main>
             <div className="card">
-                { data.map ((data) => (
-                    <div className="card__content">
+                { data.map ((data, index) => (
+                    <div key={index} className="card__content">
                         <img className="card__img" src={ data.cover } alt={ data.title }/>
                         <h3 className="card__title"> { data.title }</h3>
                     </div>
