186d4a2d2438f615a01fb37ac50ce64257040e89
Franck Cardin <franck.cardin@free.fr>
2023-06-13 02:52:52 +0200

    Modification About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,18 @@
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import BackgroundAbout from '../assets/BackgroundAbout.png';
 import Footer from '../components/Footer';
-import "../styles/about.css";
+import "../styles/about.scss";
 
 function About(){
     return (
         <main>
             <div className="about">
                 <Header />
-                <Banner />
+                <Banner BackgroundPicture= {BackgroundAbout}/>
                 <main>
                     <div className="about__content">
-
+        
                     </div>
                 </main>
                 <Footer />
