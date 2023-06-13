9ca753d4a9ec55470699f0968dd7015917412320
Franck Cardin <franck.cardin@free.fr>
2023-06-13 02:52:16 +0200

    Modification Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import Logo from '../assets/LogoHeader.png';
 import Nav from './Nav';
-import '../styles/header.css';
+import '../styles/header.scss';
 
 function Header() {
 	return (
-		<header className='header'>
-			<div>
-                <img src={Logo} alt="kasa" />
+		<header className="header">
+			<div className="header__content">
+                <img className="header__logo" src={Logo} alt="kasa" />
             </div>
         <Nav />
 		</header>
