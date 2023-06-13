2f7b4dad74fdca3c9094bd9ecaf0786214cad32d
Franck Cardin <franck.cardin@free.fr>
2023-06-13 14:28:49 +0200

    Modification About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,19 @@
+//import Composant 
 import Header from '../components/Header';
 import Banner from '../components/Banner';
-import BackgroundAbout from '../assets/BackgroundAbout.png';
 import Footer from '../components/Footer';
+//import IMG 
+import BackgroundAbout from '../assets/BackgroundAbout.png';
+//import SCSS 
 import "../styles/about.scss";
 
 function About(){
     return (
+        /* Affichage Banner avec l'img correspondante */
         <main>
             <div className="about">
                 <Header />
-                <Banner BackgroundPicture= {BackgroundAbout}/>
+                <Banner BackgroundPicture= {BackgroundAbout} alt="BannerAbout"/>
                 <main>
                     <div className="about__content">
         
