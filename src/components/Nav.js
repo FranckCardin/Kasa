fce80b078620917cf0bd9217140ee142d2359971
Franck Cardin <franck.cardin@free.fr>
2023-06-13 14:28:22 +0200

    Modification Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
+//import Composant 
 import { NavLink } from "react-router-dom";
+//import SCSS 
 import '../styles/nav.scss';
 
 function Nav(){
     return (
+        // Operateur Ternaire pour afficher l'element active
         <nav className="navbar">
             <ul className="navbar__list">
                 <li><NavLink to="/" className={(nav) => nav.isActive ? "active" : "" }>Accueil</NavLink></li>
-        
                 <li><NavLink to="/Apropos" className={(nav) => nav.isActive ? "active" : "" }>A propos</NavLink></li>
             </ul>
         </nav>
