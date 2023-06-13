cf91bc00cc74278595ec43fe586b4fa6d35b06b9
Franck Cardin <franck.cardin@free.fr>
2023-06-13 14:27:34 +0200

    Modification App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+//import Composant 
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Accommodation from './pages/Accommodation';
 import Notfound from './pages/404Error';
+//import SCSS
 import "./styles/root.scss"
 
 function App(){
     return  (
+        //Routes des différentes pages du site
         <Routes>
             <Route path="/" element = { <Home /> } />
             <Route path="/Apropos" element = { <About /> } />
