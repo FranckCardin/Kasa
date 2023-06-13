5992ebade9fdd21cfc93010852ae5c9934cad52d
Franck Cardin <franck.cardin@free.fr>
2023-06-13 14:29:08 +0200

    Modification Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
+//import Composant 
 import Header from '../components/Header';
 import Banner from '../components/Banner';
-import BackgroundBanner from '../assets/BackgroundBanner.png';
 import Card from '../components/Card';
 import Footer from '../components/Footer';
+//import Img
+import BackgroundBanner from '../assets/BackgroundBanner.png';
+
 
 function Home() {
 	return (
+		/* Affichage Banner avec l'img correspondante */
 		<div className='home'>
 			<Header />
-            <Banner BackgroundPicture={BackgroundBanner} title="Chez vous, partout et ailleurs"/>
+            <Banner BackgroundPicture={BackgroundBanner} alt="BannerHome" title="Chez vous, partout et ailleurs"/>
             <Card />
             <Footer />
 		</div>
