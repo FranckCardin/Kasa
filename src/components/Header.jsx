fb377d66dd13247b44a9d38f2fcf50db3e81e422
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:35:40 +0200

    Modification Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+//import Composant 
+import Nav from './Nav';
+//import IMG 
+import Logo from '../assets/LogoHeader.png';
+//import SCSS
+import '../styles/header.scss';
+
+function Header() {
+	return (
+		<header className="header">
+			<div className="header__content">
+                <img className="header__logo" src={Logo} alt="kasa" />
+            </div>
+        <Nav />
+		</header>
+	)
+}
+
+export default Header;
+
+
