68d9029818096d8479c170f068f054455e487f21
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:35:34 +0200

    Modification Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+//import IMG 
+import LogoFooter from "../assets/LogoFooter.png";
+//import SCSS 
+import "../styles/footer.scss"
+
+function Footer() {
+    return (
+        <div className="footer">
+            <img className="footer__logo" src={LogoFooter} alt= "Kasa" />
+            <div className="footer__copyright">© 2020 Kasa. All rights reserved</div>
+        </div>
+    )
+}
+
+export default Footer;
