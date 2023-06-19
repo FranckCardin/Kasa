8354e2b240daa34c8f597e597c89291bac33374f
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:34:26 +0200

    Modification app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+//import Composant 
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Accommodation from './pages/Accommodation';
+import Notfound from './pages/404Error';
+//import SCSS
+import "./styles/root.scss"
+
+function App(){
+    return  (
+        //Routes des différentes pages du site
+        <Routes>
+            <Route path="/" element = { <Home /> } />
+            <Route path="/Apropos" element = { <About /> } />
+            <Route path="/Logements/:id" element = { <Accommodation /> } />
+            <Route path="*" element = { <Notfound /> } />
+        </Routes>
+    );
+}
+    
+export default App;
