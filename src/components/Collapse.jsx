b67f3715a1f5721b0c2dd00efc1b6bc812cafe00
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:35:27 +0200

    Modification Collapse.jsx

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,33 @@
+//Import Composant
+import { useState } from "react";
+//Import IMG
+import Arrow from '../assets/Arrow.png';
+//Import CSS
+import '../styles/collapse.scss';
+
+function Collapse({title, content}){
+    const [isOpen, setIsOpen] = useState(false);
+
+    console.log(content);
+
+    return (
+        <div className="collapse">
+            <h3 className="collapse__title" onClick={() => setIsOpen(!isOpen)}> {title} 
+                <img className={isOpen ? 'arrow arrow_up' : 'arrow arrow_down'} 
+                     src={Arrow} 
+                     alt="Collapse Content"/> 
+            </h3>
+
+            <div className={isOpen ? "collapse__content--open" : "collapse__content--close"}>
+                {isOpen && (
+                    //Mise en tableau du props "content" pour l'affichage
+                    Array.isArray(content) ? content.map((item, index) => (
+                            <p key={index}>{item}</p>
+                    )): <p>{content}</p>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Collapse;
