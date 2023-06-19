c85e6d89abf77e2037459120d3930420e7cac076
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:34:44 +0200

    Modification Banner.jsx

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+//import SCSS 
+import '../styles/banner.scss';
+
+//Fonction avec deux props pour l'image de la bannière et le titre en fonction de la page
+function Banner ({BackgroundPicture, title, alt})  {
+    return (
+        <div className="banner">
+            <img className="banner__background" src={BackgroundPicture} alt={alt} />
+            <h1 className="banner__title">{title}</h1>
+        </div>
+    )
+}
+
+export default Banner;
