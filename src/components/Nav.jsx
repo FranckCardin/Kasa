4da8459687768ae7b660142da2578b3239f40bed
Franck Cardin <franck.cardin@free.fr>
2023-06-19 22:35:48 +0200

    Modification Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+//import Composant 
+import { NavLink } from "react-router-dom";
+//import SCSS 
+import '../styles/nav.scss';
+
+function Nav(){
+    return (
+        <nav className="navbar">
+            <ul className="navbar__list">
+                <li><NavLink to="/" className={(nav) => nav.isActive ? "active" : "" }>Accueil</NavLink></li>
+                <li><NavLink to="/Apropos" className={(nav) => nav.isActive ? "active" : "" }>A propos</NavLink></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
