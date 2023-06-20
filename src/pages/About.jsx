ac2175af62b5b30fb2a518a5249ef9f894d06b61
Franck Cardin <franck.cardin@free.fr>
2023-06-20 21:42:27 +0200

    Modification About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,7 +18,6 @@ function About(){
                         <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
                         <Collapse title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
                         <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
-
                     </div>
                 <Footer />
             </div>
