cf99c21fb4179acac6ac429dfa6793975b57fe29
Franck Cardin <franck.cardin@free.fr>
2023-06-20 21:41:38 +0200

    Modification Infos.jsx

---
+++ src/components/Infos.jsx
@@ -0,0 +1,13 @@
+//import CSS
+import "../styles/composants/infos.scss";
+
+function Infos({title, location}) {
+    return (
+        <>
+        <h1 className="accommodation__contentInfos--title">{title}</h1>
+        <p className="accommodation__contentInfos--location">{location}</p>
+        </>
+      )
+}
+
+export default Infos;
