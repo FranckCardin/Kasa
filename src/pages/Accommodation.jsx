7104ea9fa5bf3e193a0a76de5141a6e89dd90c5d
Franck Cardin <franck.cardin@free.fr>
2023-06-20 04:11:39 +0200

    Modification Accommodation.jsx

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -2,6 +2,7 @@
 import { useParams } from "react-router-dom";
 import Header from '../components/Header';
 import Slider from '../components/Slider';
+import Rating from '../components/Rating';
 import Collapse from '../components/Collapse';
 import Footer from '../components/Footer';
 //import DATA 
@@ -21,26 +22,35 @@ function Accommodation(){
         <>
         <Header />
             <main className="main__accommodation">
-                <div className="Slider">
+                <section className="Slider">
                     <Slider/>
-                </div>
-                <div className="accommodation">
+                </section>
+                
+                <section className="accommodation">
                     <div className="accommodation__content">
-                    <h1 className="accommodation__content--title">{dataCurrentAccomodation[0].title}</h1>
-                    <p className="accommodation__content--location">{dataCurrentAccomodation[0].location}</p>
-                    <div className="accommodation__tags">
-                        {dataCurrentAccomodation[0].tags.map((tag) => {
-                                    return (
-                                        <button className="accommodation__tags--btn">{tag}</button>
-                                    )
-                                })}
+                        <div className="accommodation__contentInfos">
+                            <h1 className="accommodation__contentInfos--title">{dataCurrentAccomodation[0].title}</h1>
+                            <p className="accommodation__contentInfos--location">{dataCurrentAccomodation[0].location}</p>
+                        </div>
+                        <div className="accommodation__host">
+                            <p className="accommodation__host--name">{dataCurrentAccomodation[0].host.name}</p>
+                            <img className="accommodation__host--picture"src={dataCurrentAccomodation[0].host.picture} alt="Hébergeur du logement"></img>
+                        </div>
                     </div>
+
+                    <div className="accommodation__contentTagsRatings">
+                        <div className="accommodation__contentTagsRatings--tags">
+                            {dataCurrentAccomodation[0].tags.map((tag) => {
+                                        return (
+                                            <button className="accommodation__contentTagsRatings--tags--btn">{tag}</button>
+                                        )
+                                    })}
+                        </div>
+                        <div className="accommodation__contentTagsRatings--ratings">
+                            < Rating rating={dataCurrentAccomodation[0].rating}/>
+                        </div>
                     </div>
-                    <div className="accommodation__host">
-                        <p className="accommodation__host--name">{dataCurrentAccomodation[0].host.name}</p>
-                        <img className="accommodation__host--picture"src={dataCurrentAccomodation[0].host.picture} alt="Hébergeur du logement"></img>
-                    </div>
-                </div>
+                </section>
             
 
                 <div className="collapse__accommodation">
