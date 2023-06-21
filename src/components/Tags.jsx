1b0346fcabb711fdea5f3483b79426c6a7ebc067
Franck Cardin <franck.cardin@free.fr>
2023-06-21 16:00:23 +0200

    Modification Tags.jsx

---
+++ src/components/Tags.jsx
@@ -0,0 +1,12 @@
+//import CSS
+import "../styles/composants/tags.scss";
+
+function Tags({tag}) {
+    return (
+        <button className="accommodation__contentTagsRatings--tags--btn">{tag}</button>
+  );
+}
+
+export default Tags;
+
+
