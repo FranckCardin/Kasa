ab04be67a40dca648660f497cac06887c70b0b52
Franck Cardin <franck.cardin@free.fr>
2023-06-25 12:46:51 +0200

    Modification App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-//import Composant 
+//import HOOK REACT
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+//import COMPOSANT 
 import Home from './pages/Home';
 import About from './pages/About';
 import Accommodation from './pages/Accommodation';
@@ -11,11 +12,13 @@ import "./styles/root.scss"
 function App(){
     return  (
         //Routes des différentes pages du site
+        //Si page non existante, redirection vers la page NotFound
         <Routes>
             <Route path="/" element = { <Home /> } />
             <Route path="/Apropos" element = { <About /> } />
             <Route path="/Logements/:id" element = { <Accommodation /> } />
-            <Route path="*" element = { <Notfound /> } />
+            <Route path="/PageNotFound" element = { <Notfound /> } />
+            <Route path="*" element={<Navigate to="/PageNotFound" />} />
         </Routes>
     );
 }
