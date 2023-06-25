af4f3e3d78a23e1ce8df19f753012f887080b7d8
Franck Cardin <franck.cardin@free.fr>
2023-06-25 12:47:27 +0200

    Modification Collapse.jsx

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -2,18 +2,19 @@
 import { useState } from "react";
 //Import IMG
 import Arrow from '../assets/Arrow.png';
-//Import CSS
+//Import SCSS
 import '../styles/composants/collapse.scss';
 
-function Collapse({title, content}){
+function Collapse({title, content}){ 
+    //Utilisation du useState pour stocker l'état en false
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="collapse">
-            <h3 className="collapse__title" onClick={() => setIsOpen(!isOpen)}> {title} 
+            <h3 className="collapse__title" onClick={() => setIsOpen(!  isOpen)}> {title} 
                 <img className={isOpen ? 'arrow arrow_up' : 'arrow arrow_down'} 
                      src={Arrow} 
-                     alt="Collapse Content"/> 
+                     alt="Contenu de la collapse" /> 
             </h3>
 
             <div className={isOpen ? "collapse__content--open" : "collapse__content--close"}>
