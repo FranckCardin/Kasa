f0376602fb1d8247c9dc28d47e15e500c6d5abd7
Franck Cardin <franck.cardin@free.fr>
2023-06-25 12:47:59 +0200

    Modification Host.jsx

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -1,11 +1,12 @@
-//import CSS
+//import SCSS
 import "../styles/composants/host.scss";
 
 function Host({name, picture}) {
   return (
+    //Split de la data name pour le nom et le prénom
     <>
-    <h3 className="accommodation__host--title"><span>{name.split(' ')[0]}</span> <span>{name.split(' ')[1]}</span></h3>
-    <img className="accommodation__host--picture" src={picture} alt={`avatar de ${name}`} />
+    <h3 className="accommodation__contentHostRatings--host--title"><span>{name.split(' ')[0]}</span> <span>{name.split(' ')[1]}</span></h3>
+    <img className="accommodation__contentHostRatings--host--picture" src={picture} alt={`avatar de ${name}`} />
     </>
   )
 }
